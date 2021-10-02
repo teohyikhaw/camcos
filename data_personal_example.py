266e364d0b359f73612880d68fd39479bdf44cef
Jacob McGraw <83054991+Jacob-the-Numberphile@users.noreply.github.com>
2021-10-01 18:39:17 -0700

    Update data_personal_example.py

--- data_personal_example.py
+++ data_personal_example.py
@@ -1,6 +1,11 @@
 # Make a file called data_personal.py with a variation of the following file.
 # Do NOT check in that file into git!
 
+#Links:
+#transaction_data_file -> https://drive.google.com/file/d/1MdeFC5beOPmaDJEnaCVgfb-nGFEYb8KQ/view?usp=sharing
+#block_data_file -> https://drive.google.com/file/d/1CW3FtaSARnUKHHociOOe65zq61IRvDSx/view?usp=sharing
+#large_pre_gas_prices_file -> https://drive.google.com/file/d/1Ii_cuSeHUufMrfyHTs-j6IbmWMybkSNO/view?usp=sharing
+
 transaction_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/txs_12965_12967.csv"
 block_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/bxs_12965_12967.csv"
 large_pre_gas_prices_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/03_22_03_26.csv"
