cd04375a56344912d10e47c39c01278db2da2fef
Yizzle <krzhang@gmail.com>
2021-10-01 17:21:31 -0700

    separate personal data into own file

---
+++ data_personal_example.py
@@ -0,0 +1,6 @@
+# Make a file called data_personal with a variation of the following file.
+# Do NOT check in that file into git!
+
+transaction_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/txs_12965_12967.csv"
+block_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/bxs_12965_12967.csv"
+large_pre_gas_prices_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/03_22_03_26.csv"
