5633da80c4ab3b1762c5de7ebd5d1a0f94b75b18
Jacob McGraw <83054991+Jacob-the-Numberphile@users.noreply.github.com>
2021-10-20 14:35:56 -0700

    valuations
    this code determines the rescaled valuations based on the mechanism that i mentioned earlier in the discord, the new stuff starts on line 91, the data cleaning code was redone from earlier work

---
+++ pre_and_post_valuations_data.py
@@ -0,0 +1,16 @@
+ ##Links to data sources:
+
+## 1:
+
+##transactions:https://drive.google.com/file/d/1MdeFC5beOPmaDJEnaCVgfb-nGFEYb8KQ/view?usp=sharing
+
+## 2:
+
+##block_data:https://drive.google.com/file/d/1CW3FtaSARnUKHHociOOe65zq61IRvDSx/view?usp=sharing
+
+
+
+#change the path to your specific machine:)
+## 1:
+transaction_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/txs_12965_12967.csv"
+block_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/bxs_12965_12967.csv"
