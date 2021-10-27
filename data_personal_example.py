3cc0d0e932e0f304a289e54c15d62c52716a3aca
Jacob McGraw <83054991+Jacob-the-Numberphile@users.noreply.github.com>
2021-10-26 19:11:11 -0700

    This updates the variance checker to include the suite spot data

--- data_personal_example.py
+++ data_personal_example.py
@@ -6,6 +6,10 @@
 #block_data_file -> https://drive.google.com/file/d/1CW3FtaSARnUKHHociOOe65zq61IRvDSx/view?usp=sharing
 #large_pre_gas_prices_file -> https://drive.google.com/file/d/1Ii_cuSeHUufMrfyHTs-j6IbmWMybkSNO/view?usp=sharing
 
-transaction_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/txs_12965_12967.csv"
-block_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/bxs_12965_12967.csv"
-large_pre_gas_prices_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/03_22_03_26.csv"
+transaction_data_file ="/Volumes/Library/CAMCOS_DATASETS/txs_12965_12967.csv"
+block_data_file = "/Volumes/Library/CAMCOS_DATASETS/bxs_12965_12967.csv"
+large_pre_gas_prices_file = "/Volumes/Library/CAMCOS_DATASETS/03_22_03_26.csv"
+
+#suite spot data
+suite_spot_txn="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/txns_12475700_12491800.csv"
+suite_spot_blx="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/blks_12475700_12491800.csv"
