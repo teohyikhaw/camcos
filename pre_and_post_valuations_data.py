c4b8bc14ae139606aee87734c88d7d4978653428
Jacob McGraw <83054991+Jacob-the-Numberphile@users.noreply.github.com>
2021-10-26 23:41:13 -0700

    "suite spot" addendum
    adds the "suite spot" data to the Pre and post valuations notebook

--- pre_and_post_valuations_data.py
+++ pre_and_post_valuations_data.py
@@ -19,7 +19,12 @@
 
 
 #change the path to your specific machine:)
-transaction_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/txs_12965_12967.csv"
-block_data_file = "/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/bxs_12965_12967.csv"
-pre_geth_file="/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/03_22_03_26.csv"
-geth_reccomendation_file="/Users/jacobmcgraw/Downloads/CAMCOS_Datasets/pre_geth_predictions.csv"
+transaction_data_file = "/Volumes/Library/CAMCOS_DATASETS/txs_12965_12967.csv"
+block_data_file = "/Volumes/Library/CAMCOS_DATASETS/bxs_12965_12967.csv"
+pre_geth_file="/Volumes/Library/CAMCOS_DATASETS/03_22_03_26.csv"
+geth_reccomendation_file="/Volumes/Library/CAMCOS_DATASETS/pre_geth_predictions.csv"
+
+
+#suite spot data
+suite_spot_txn="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/txns_12475700_12491800.csv"
+suite_spot_blx="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/blks_12475700_12491800.csv"
