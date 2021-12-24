25ca569912661dd3a3af1dbc286defa08bcffc0c
Jacob McGraw <83054991+Jacob-the-Numberphile@users.noreply.github.com>
2021-12-24 08:51:01 -0800

    organized files
    increased code readability and organization

---
+++ Jacob_code/Junk_code/data_personal_example.py
@@ -0,0 +1,32 @@
+# Make a file called data_personal.py with a variation of the following file.
+# Do NOT check in that file into git!
+
+#Links:
+#transaction_data_file -> https://drive.google.com/file/d/1MdeFC5beOPmaDJEnaCVgfb-nGFEYb8KQ/view?usp=sharing
+#block_data_file -> https://drive.google.com/file/d/1CW3FtaSARnUKHHociOOe65zq61IRvDSx/view?usp=sharing
+#large_pre_gas_prices_file -> https://drive.google.com/file/d/1Ii_cuSeHUufMrfyHTs-j6IbmWMybkSNO/view?usp=sharing
+
+#post-eip data
+transaction_data_file ="/Volumes/Library/CAMCOS_DATASETS/txs_12965_12967.csv"
+block_data_file = "/Volumes/Library/CAMCOS_DATASETS/bxs_12965_12967.csv"
+#pre-eip data DAY ONE
+large_pre_gas_prices_file = "/Volumes/Library/CAMCOS_DATASETS/03_22_03_26.csv"
+
+#suite spot data
+suite_spot_txn="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/txns_12475800_12491299.csv"
+suite_spot_blx="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/blks_12475800_12491299.csv"
+
+#data requested by Randy
+post_eip_large_blocks="/Volumes/Library/CAMCOS_DATASETS/randy_block_data/post_eip.csv"
+
+##Data for post eip disregarding the blocks with spikes
+new_post_eip_txn="/Volumes/Library/CAMCOS_DATASETS/pre_eip_data_no_spikes/transactions_cut.csv"
+new_post_eip_blx="/Volumes/Library/CAMCOS_DATASETS/pre_eip_data_no_spikes/blocks_cut.csv"
+new_post_eip_rcpts="/Volumes/Library/CAMCOS_DATASETS/pre_eip_data_no_spikes/receipts_cut.csv"
+
+#Data to answer question for Zhang
+zhang_txn="/Volumes/Library/CAMCOS_DATASETS/zhang_txn.csv"
+
+#pre-eip data LATE OCTOBER
+post_eip_oct_txn_file="/Volumes/Library/CAMCOS_DATASETS/post_eip_oct_txns.csv"
+post_eip_oct_blx_file="/Volumes/Library/CAMCOS_DATASETS/post_eip_oct_blx.csv"
---
+++ Jacob_code/Junk_code/pre_and_post_valuations_data.py
@@ -0,0 +1,38 @@
+ ##Links to data sources respectively:
+
+## 1:
+
+##transactions:https://drive.google.com/file/d/1MdeFC5beOPmaDJEnaCVgfb-nGFEYb8KQ/view?usp=sharing
+
+## 2:
+
+##block_data:https://drive.google.com/file/d/1CW3FtaSARnUKHHociOOe65zq61IRvDSx/view?usp=sharing
+
+## 3: 
+
+##https://drive.google.com/file/d/1UX5bHnqrGvChGaaRDO7VFdcbssRmJnms/view?usp=sharing
+
+## 4: 
+
+##https://drive.google.com/file/d/1Ii_cuSeHUufMrfyHTs-j6IbmWMybkSNO/view?usp=sharing
+
+
+
+#change the path to your specific machine:)
+transaction_data_file = "/Volumes/Library/CAMCOS_DATASETS/txs_12965_12967.csv"
+block_data_file = "/Volumes/Library/CAMCOS_DATASETS/bxs_12965_12967.csv"
+pre_geth_file="/Volumes/Library/CAMCOS_DATASETS/03_22_03_26.csv"
+geth_reccomendation_file="/Volumes/Library/CAMCOS_DATASETS/pre_geth_predictions.csv"
+
+
+#suite spot data
+suite_spot_txn="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/txns_12475800_12491299.csv"
+suite_spot_blx="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/blks_12475800_12491299.csv"
+
+
+#pre-eip data LATE OCTOBER
+post_eip_oct_txn_file="/Volumes/Library/CAMCOS_DATASETS/post_eip_oct_txns.csv"
+post_eip_oct_blx_file="/Volumes/Library/CAMCOS_DATASETS/post_eip_oct_blx.csv"
+
+#october block data
+october_block_data="/Volumes/Library/CAMCOS_DATASETS/oct.csv"
