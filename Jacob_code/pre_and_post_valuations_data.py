59e187b1337e4f46fc4aac34c3355996448eb37a
Jacob McGraw <83054991+Jacob-the-Numberphile@users.noreply.github.com>
2022-01-03 13:16:25 -0800

    Added Randy's Code
    Added Randy's code and cleaned the data files for readability for future users

--- Jacob_code/pre_and_post_valuations_data.py
+++ Jacob_code/pre_and_post_valuations_data.py
@@ -1,38 +1,43 @@
- ##Links to data sources respectively:
-
-## 1:
-
-##transactions:https://drive.google.com/file/d/1MdeFC5beOPmaDJEnaCVgfb-nGFEYb8KQ/view?usp=sharing
-
-## 2:
-
-##block_data:https://drive.google.com/file/d/1CW3FtaSARnUKHHociOOe65zq61IRvDSx/view?usp=sharing
-
-## 3: 
-
-##https://drive.google.com/file/d/1UX5bHnqrGvChGaaRDO7VFdcbssRmJnms/view?usp=sharing
-
-## 4: 
-
-##https://drive.google.com/file/d/1Ii_cuSeHUufMrfyHTs-j6IbmWMybkSNO/view?usp=sharing
-
-
-
-#change the path to your specific machine:)
+ ##Link to data sources: https://drive.google.com/drive/u/1/folders/1xskDSaCgMP64lQnkxMnTWbHwFo4NnArI
+    
+    
+    
+#Change the path on your specific machine:)
+    
+    
+#Specific name and location of used datasets 
+      
+## 1: Day one of EIP 1559 transactions
 transaction_data_file = "/Volumes/Library/CAMCOS_DATASETS/txs_12965_12967.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->Post EIP-1559 data->txs_12965_12967.csv
+
+
+## 2: Day one of EIP 1559 blocks
 block_data_file = "/Volumes/Library/CAMCOS_DATASETS/bxs_12965_12967.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->Post EIP-1559 data->bxs_12965_12967.csv
+
+
+## 3: Pre EIP 1559 transactions
 pre_geth_file="/Volumes/Library/CAMCOS_DATASETS/03_22_03_26.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->03_22_03_26.csv
+
+
+## 4: collabed with code from Randy to get pre geth predictions
 geth_reccomendation_file="/Volumes/Library/CAMCOS_DATASETS/pre_geth_predictions.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->pre_geth_predictions.csv
 
 
-#suite spot data
+## 5: "suite spot" transactions and blocks, largely unused (yes that is a horrible spelling mistake)
 suite_spot_txn="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/txns_12475800_12491299.csv"
 suite_spot_blx="/Volumes/Library/CAMCOS_DATASETS/suite_spot_data/blks_12475800_12491299.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->Suite Spot Data
 
 
-#pre-eip data LATE OCTOBER
+## 6: post-eip transaction data LATE OCTOBER
 post_eip_oct_txn_file="/Volumes/Library/CAMCOS_DATASETS/post_eip_oct_txns.csv"
-post_eip_oct_blx_file="/Volumes/Library/CAMCOS_DATASETS/post_eip_oct_blx.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->Post EIP-1559 data->transactions-13360000-13365000.csv.zip
 
-#october block data
+
+## 7: post-eip block data LATE OCTOBER
 october_block_data="/Volumes/Library/CAMCOS_DATASETS/oct.csv"
+##Location: CAMCOS Spring 2021->relavant datasets->blx_for_tien->oct.csv
