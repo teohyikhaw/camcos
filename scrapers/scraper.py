52da95a4c92c38bdfc91492c5d1a85b7af8063f2
Yik Haw Teoh <teohyikhaw@gmail.com>
2022-11-29 14:03:49 -0800

    Added comments to scraper.py

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -2,30 +2,18 @@ from web3 import Web3, EthereumTesterProvider
 import pandas as pd
 
 if __name__ == "__main__":
+    # Setup RPC endpoint, here we are using a public one fro ankr
     web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
-
-
-    # df = pd.read_csv("blockData.csv")
-    # df2 = pd.read_csv("transactionData.csv")
-
-    s = 0
 
     block_data = []
     transaction_data = []
-    # 15817058
-    for blockNumber in range(15627832, 15627932, 1):
+    startBlockNum = 15627832
+    endBlockNum = 15627932
+    stepCount = 1
+
+    for blockNumber in range(startBlockNum, endBlockNum, stepCount):
         block = web3.eth.get_block(blockNumber)
         print(block)
-        # df.loc[blockNumber + 1] = [
-        #     blockNumber,
-        #     block.gasLimit,
-        #     block.gasUsed,
-        #     block.baseFeePerGas,
-        #     block.difficulty,
-        #     block.hash.hex(),
-        #     [x.hex() for x in block.transactions]
-        # ]
-        # df.to_csv("blockData.csv", index=False)
 
         block_data_single = {
             "blockNumber":blockNumber,
@@ -38,22 +26,9 @@ if __name__ == "__main__":
         }
         block_data.append(block_data_single)
 
+        # Loops through all transactions in the block
         for i in range(len(block.transactions)):
             tx = web3.eth.get_transaction(block.transactions[i])
-
-            # df2.loc[s] = [
-            #     blockNumber,
-            #     tx.gas,
-            #     tx.gasPrice,
-            #     tx.input,
-            #     len(tx.input),
-            #     tx.nonce,
-            #     tx.to,
-            #     tx["from"]
-            # ]
-            # df2.to_csv("transactionData.csv", index=False)
-            # s+=1
-
             transaction_data_single = {
                 "blockNumber":blockNumber,
                 "gas":tx.gas,
