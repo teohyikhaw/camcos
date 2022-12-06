bc57f73f51dec75d96512d8b1952c619064aadd5
krzhang <krzhang@gmail.com>
2022-12-06 13:07:23 -0800

    simulate uniform and Nash symmetric auctions

--- auctions/simulator.py
+++ auctions/simulator.py
@@ -1,10 +1,42 @@
-import numpy
+import numpy as np
+import scipy as sp
 
-class Agent(object):
+class Distribution(object):
   """
-  A single agent, which is characterized by a distribution.
+  A thin wrapper to sample from a distribution.
   """
-  pass
+  def sample(self, k):
+    """ return k samples, in a list"""
+    pass
+
+  def nash(self, k, value):
+    """ given a valuation, how to bid when there are k players?"""
+    pass
+
+class UniformDistribution(Distribution):
+  def __init__(self, low, high):
+    self.low = low
+    self.high = high
+    
+  def sample(self, k):
+    return np.random.uniform(self.low, self.high, k)
+
+  def nash(self, k, value):
+    integral = sp.quad(lambda x: pow((x-self.low)/(value - self.low), k-1), 0, value)
+    return value - integral
+  
+class GammaDistribution(Distribution):
+  def __init__(self, shape, scale):
+    self.shape = shape
+    self.scale = scale
+
+  def sample(self, k):
+    return np.random.gamma(self.shape, self.scale, k)
+
+  def nash(self, k, value):
+    cdf = lambda x: sp.stats.gamma.cdf(x, self.shape, scale=self.scale)
+    integral = sp.quad(lambda x: pow(cdf(x)/cdf(value), k-1), 0, value)
+    return value - integral
 
 class Simulator(object):
   """
@@ -26,10 +58,17 @@ class SymmetricSimulator(Simulator):
     self.num_agents = num_agents
     self.distribution = distribution
 
-  def simulate_turn(self):
-    valuations = [self.sample_signal(self.distribution) for i in range(self.num_agents)]
-    bids = [self.sample_bid(s) for s in valuations]
-    winning_index = numpy.argmax(bids)
+  def simulate_single_turn(self):
+    valuations = self.distribution.sample(self.num_agents)
+    bids = [self.distribution.nash(s) for s in valuations]
+    winning_index = np.argmax(bids)
     revenue = bids[winning_index]
     winner_gain = valuations[winning_index] - revenue
     return (valuations, bids, revenue, winner_gain)
+
+  def simulate_turns(self, k):
+    data = [self.simulate_single_turn() for i in range(k)]
+    revenues = [d[2] for d in data]
+    winner_gains = [d[3] for d in data]
+    print("average revenue: {}" % sum(revenues)/k)
+    print("average gains: {}" % sum(winner_gains)/k)
