5386cc4064babb80fa2f0344db182b424519c5ec
teohyikhaw <teohyikhaw@gmail.com>
2022-12-11 21:55:34 -0800

    Updated shell script

--- simulations/shellscripts/run_simulator.sh
+++ simulations/shellscripts/run_simulator.sh
@@ -13,8 +13,17 @@ export PYTHONPATH=/home/yteoh/camcos/
 call_data_standard_value=25
 call_data_learning_rate=1/8
 call_data_target_limit=25000
-step_count=10
-directory="small_test"
+step_count=400
+directory="new_results"
 
-srun --ntasks 8 python /home/yteoh/camcos/simulations/shellscripts/single_trajectory_simulation.py $call_data_standard_value $call_data_learning_rate $call_data_target_limit $step_count $directory
-srun --ntasks 1 python /home/yteoh/camcos/simulations/shellscripts/average_trajectories.py $call_data_standard_value $call_data_learning_rate $call_data_target_limit $step_count $directory
+call_data_learning_rates=("1/13" "1/12" "1/11" "1/10" "1/9")
+call_data_target_limits=("21500" "22000" "22500" "23000" "23500" "24000" "24500" "25000")
+
+for i in "${call_data_learning_rates[@]}"
+do
+  for j in "${call_data_target_limits[@]}"
+  do
+  srun --ntasks 8 python /home/yteoh/camcos/simulations/shellscripts/single_trajectory_simulation.py $call_data_standard_value "$i" "$j" $step_count $directory
+  srun --ntasks 1 python /home/yteoh/camcos/simulations/shellscripts/average_trajectories.py $call_data_standard_value "$i" "$j" $step_count $directory
+  done
+done
