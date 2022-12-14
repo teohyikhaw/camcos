a83828168de33bd6d4bd77c53a250371111c79dc
krzhang <krzhang@gmail.com>
2022-12-13 17:33:09 -0800

    add ASP simulation

--- auctions/simulator.py
+++ auctions/simulator.py
@@ -17,12 +17,15 @@ class UniformDistribution(Distribution):
   def __init__(self, low, high):
     self.low = low
     self.high = high
+
+  def __str__(self):
+    return "uniform"
     
   def sample(self, k):
     return np.random.uniform(self.low, self.high, k)
 
   def nash(self, k, value):
-    integral = sp.integrate.quad(lambda x: pow((x-self.low)/(value - self.low), k-1), 0, value)
+    integral = sp.integrate.quad(lambda x: pow((x-self.low)/(value - self.low + 0.000000001), k-1), self.low + 0.0000000001, value)
     return value - integral[0]
   
 class GammaDistribution(Distribution):
@@ -30,6 +33,9 @@ class GammaDistribution(Distribution):
     self.shape = shape
     self.scale = scale
 
+  def __str__(self):
+    return "gamma"
+    
   def sample(self, k):
     return np.random.gamma(self.shape, self.scale, k)
 
@@ -59,16 +65,26 @@ class SymmetricSimulator(Simulator):
     self.distribution = distribution
 
   def simulate_single_turn(self):
+    """ simulate a single turn, returning all relevant data"""
     valuations = self.distribution.sample(self.num_agents)
-    bids = [self.distribution.nash(s) for s in valuations]
+    bids = [self.distribution.nash(self.num_agents, s) for s in valuations]
     winning_index = np.argmax(bids)
     revenue = bids[winning_index]
     winner_gain = valuations[winning_index] - revenue
     return (valuations, bids, revenue, winner_gain)
 
-  def simulate_turns(self, k):
+  def simulate_turns(self, k, as_percentile=95):
     data = [self.simulate_single_turn() for i in range(k)]
     revenues = [d[2] for d in data]
+    average_bid = np.average([np.average(d[1]) for d in data])
+    # auction-smashing bid
+    ASB = np.percentile(revenues, as_percentile)
+    ASP = ASB - average_bid
     winner_gains = [d[3] for d in data]
-    print("average revenue: {}" % sum(revenues)/k)
-    print("average gains: {}" % sum(winner_gains)/k)
+    print("{} {} agents".format(self.num_agents, self.distribution))
+    print("average revenue: {}".format(sum(revenues)/k))
+    print("average gains: {}".format(sum(winner_gains)/k))
+    print("average bid: {}".format(average_bid))
+    print("ASB: {}".format(ASB))
+    print("ASP: {}".format(ASP))
+    return data
