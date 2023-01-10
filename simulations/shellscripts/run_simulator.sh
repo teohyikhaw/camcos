b0999a57557c20220db3e902bd3ea5ece6779760
teohyikhaw <teohyikhaw@gmail.com>
2023-01-11 00:22:12 +0800

    Shellscript changes

--- simulations/shellscripts/run_simulator.sh
+++ simulations/shellscripts/run_simulator.sh
@@ -14,12 +14,12 @@ call_data_standard_value=25
 call_data_learning_rate=1/8
 call_data_target_limit=25000
 step_count=400
-directory="2023_01_10_1"
+directory="2023_01_11"
 filename="specialGeneration.csv"
 
 #call_data_learning_rates=("1/8" "3/8" "5/8")
 call_data_standard_values=("25")
-call_data_target_limits=("20000" "25000" "30000" "50000" "70000" "90000")
+call_data_target_limits=("72500" "75000" "77500" "80000" "82000" "85000")
 
 for i in "${call_data_standard_values[@]}"
 do
